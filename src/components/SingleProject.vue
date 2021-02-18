ae42102cae768ea4e981619e10cb8fab524ed607
ahmedhabeeb73 <alnaib888@gmail.com>
2021-02-18 11:05:35 +0100

    del project and completing pro

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{complete:project.complete}">
     <div class="actions">
       <h3 @click="showDetails" title="show details">{{project.title}}</h3>
       <div class="icons">
        <i class="far fa-edit fa-2x"></i>
-       <i class="far fa-trash-alt fa-2x"></i>
-       <i class="far fa-check-circle fa-2x"></i>
+       <i @click="deleteProject" class="far fa-trash-alt fa-2x"></i>
+       <i @click="toggleComplete" class="far fa-check-circle fa-2x tick"></i>
       </div>
     </div>
     <div v-if="details" class="details">
@@ -21,11 +21,30 @@ props:['project'],
 data(){
   return {
     details:false,
+    url:'http://localhost:3000/projects/' +this.project.id,
   }
 },
 methods:{
   showDetails(){
     this.details=!this.details
+  },
+  deleteProject(){
+    fetch(this.url,{method:"DELETE"})
+    .then(() => this.$emit('delete',this.project.id))
+    .catch(err => console.log(err.message))
+    
+  },
+  toggleComplete()
+  {
+    fetch(this.url,{
+      method:"PUT",
+      headers:{'Content-type':'application/json'},
+      body:JSON.stringify({complete:!this.project.complete})
+      }).then(()=>{
+        this.$emit('complete',this.project.id)
+        .catch(err => console.log(err.message))
+      })
+
   }
 }
 }
@@ -39,6 +58,13 @@ methods:{
   border-radius: 4px;
   box-shadow: 1px 2px 3px rgb(0, 0, 0,0.05);
   border-left: 4px solid #e90074;
+}
+.complete{
+  border-left:4px solid #00ce89 ;
+}
+
+.project.complete .tick {
+  color: #00ce89;
 }
 h3 {
   cursor: pointer;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
   <div v-if="projects.length">
     
      <div v-for="project in projects" :key="project.id">
-      <SingleProject :project="project"/>
+      <SingleProject :project="project" @delete="handleDelete" @complete='handleComplete'/>
      </div>
      
   </div>
@@ -33,6 +33,19 @@ export default {
     .then(data => this.projects = data)
     .catch(err => console.log(err.message));
     
+  },
+  methods:{
+    handleDelete(id){
+      this.projects=this.projects.filter((project)=>{
+        return project.id !== id
+      })
+    },
+    handleComplete(id){
+      let p= this.projects.find((project)=>{
+           return project.id ===id
+      })
+      p.complete=!p.complete
+    }
   }
 }
 </script>
